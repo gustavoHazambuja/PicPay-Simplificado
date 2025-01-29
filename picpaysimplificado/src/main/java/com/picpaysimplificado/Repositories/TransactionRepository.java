e45134db1a184329d04d9b157256505851056297
Gustavo Henrique <guazambujinha@gmail.com>
2025-01-29 19:58:48 -0300

    transaction reporitoy add

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/Repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.picpaysimplificado.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.picpaysimplificado.Entities.Transaction.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    
+}
