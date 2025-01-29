26d064352a551f0ab0bbd38f346e354518e36b55
Gustavo Henrique <guazambujinha@gmail.com>
2025-01-29 19:55:20 -0300

    reposiroty user add

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/Repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.picpaysimplificado.Repositories;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.picpaysimplificado.Entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+}
